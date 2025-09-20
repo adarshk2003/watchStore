e6968c7e507a095a2f87943589a875a7494bafe7
adarsh k <reaemp4@gmail.com>
2025-09-20 16:00:25 +0100

    Update NavAdminCommon.jsx

--- client/src/components/adminComponent/NavAdminCommon.jsx
+++ client/src/components/adminComponent/NavAdminCommon.jsx
@@ -91,9 +91,13 @@ function NavAdminCommon() {
           <FontAwesomeIcon icon={faUser} /> Login
         </li>
       </Link>
-              <li className="list-none w-full text-center p-4 hover:bg-emerald-900 text-white transition-all cursor-pointer">
-          <FontAwesomeIcon icon={faUser}  onClick={logout}/> Logout
-        </li>
+       <li
+  className="list-none w-full text-center p-4 hover:bg-emerald-900 text-white transition-all cursor-pointer"
+  onClick={logout}  
+>
+  <FontAwesomeIcon icon={faUser} /> Logout
+</li>
+
     </div>
   </div>
 </>
