b40df449aae90c8d5e0bdc1537de7041c96e4756
adarsh k <reaemp4@gmail.com>
2025-09-20 16:11:27 +0100

    Update NavSellerCom.jsx

--- client/src/components/sellerComponent/NavSellerCom.jsx
+++ client/src/components/sellerComponent/NavSellerCom.jsx
@@ -60,8 +60,9 @@ function NavSellerCom() {
         </i>
 
        <div
-  className={`absolute md:hidden top-24 left-0 w-full bg-black flex flex-col items-center gap-6 font-semibold text-lg transform transition-transform ${
-    isMenuOpen ? "opacity-100" : "opacity-0"
+<div
+  className={`fixed md:hidden top-0 left-0 w-full h-full bg-black flex flex-col items-center gap-6 font-semibold text-lg transform transition-transform ${
+    isMenuOpen ? "opacity-100 translate-y-0" : "opacity-0 -translate-y-full"
   }`}
   style={{ zIndex: 50, transition: "transform 0.3s ease, opacity 0.3s ease" }}
 >
