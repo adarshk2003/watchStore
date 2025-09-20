c5d29808073ae33c92add471d8fb4829cdd4dd02
adarsh <reaemp4@gmail.com>
2025-09-20 10:16:52 +0100

    final added

--- server/db/seeders/20241210164215-user.js
+++ server/db/seeders/20241210164215-user.js
@@ -1,31 +1,29 @@
 'use strict';
-const bcrypt = require('bcrypt');
-// const user_type = require('../models/user_type');
+const bcrypt = require('bcryptjs');  
+
 module.exports = {
   up: (models, mongoose) => {
-    let password = "admin@123";
-    let salt = bcrypt.genSaltSync(10);
-    let hashed_pass = bcrypt.hashSync(password, salt);
+    const password = "admin@123";
+    const hashed_pass = bcrypt.hashSync(password, 10);
 
     return models.users.insertMany([
       {
-        "name": "admin",
-        "email": "admin@gmail.com",
-        "password": hashed_pass,
-        "user_type": "675870dbae9c6b1dffa1a7ed"
+        name: "admin",
+        email: "admin@gmail.com",
+        password: hashed_pass,
+        user_type: new mongoose.Types.ObjectId("675870dbae9c6b1dffa1a7ed")
       }
     ]).then(res => {
-      console.log(res.insertedCount);
-    })
+      console.log("Inserted:", res.length);
+    });
   },
 
   down: (models, mongoose) => {
-    return models.users.deleteMany(
-      {
-        _id: "675870dbae9c6b1dffa1a7ed"
-      }
-    ).then(res => {
-      console.log(res.deletedCount);
-    })
+    return models.users.deleteMany({
+      email: "admin@gmail.com"
+    }).then(res => {
+      console.log("Deleted:", res.deletedCount);
+    });
   }
 };
+
